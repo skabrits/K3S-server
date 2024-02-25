70e793c297b25ed6997a1552b4d8c819e7605f45
Sevasuslik <seva.kabrits@gmail.com>
2024-02-26 01:38:42 +0300

    fix

--- vms/scripts/common.sh
+++ vms/scripts/common.sh
@@ -5,6 +5,10 @@ sudo swapoff -a
 # keeps the swaf off during reboot
 (crontab -l 2>/dev/null; echo "@reboot /sbin/swapoff -a") | crontab - || true
 
+sudo growpart /dev/sda 3
+sudo lvextend -l +100%FREE /dev/mapper/ubuntu--vg-ubuntu--lv
+sudo resize2fs /dev/mapper/ubuntu--vg-ubuntu--lv
+
 sudo apt update && sudo apt install -y avahi-daemon avahi-autoipd libnss-mdns avahi-utils ntp
 
 sudo ufw disable
