c3357123653763f9b0d671cb35a730f1d28ccc10
skabrits <seva.kabrits@gmail.com>
2024-06-17 23:40:42 +0300

    Update common.sh

--- vms/scripts/common.sh
+++ vms/scripts/common.sh
@@ -9,6 +9,10 @@ sudo growpart /dev/sda 3
 sudo lvextend -l +100%FREE /dev/mapper/ubuntu--vg-ubuntu--lv
 sudo resize2fs /dev/mapper/ubuntu--vg-ubuntu--lv
 
-sudo apt update && sudo apt install -y avahi-daemon avahi-autoipd libnss-mdns avahi-utils ntp
+sudo apt update && sudo apt install -y avahi-daemon avahi-autoipd libnss-mdns avahi-utils ntp net-tools
 
 sudo ufw disable
+
+sudo route add default gw 192.168.0.1
+
+sudo `route -n | awk '{ if ($8 =="eth0" && $2 != "0.0.0.0") print "route del default gw " $2; }'`
