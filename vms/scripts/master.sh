9637f71e0c10f04f95c2da9c4fbfb6a0a19cba0b
skabrits <seva.kabrits@gmail.com>
2024-06-22 02:35:25 +0300

    Update master.sh

--- vms/scripts/master.sh
+++ vms/scripts/master.sh
@@ -17,7 +17,7 @@ sudo echo "nameserver $(echo $IPADDR | cut -d "." -f1-3).1" >> /etc/resolv.conf
 
 LBIP="$(avahi-resolve -n4 master-01.local | awk '{print $2}')"
 
-curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="777" INSTALL_K3S_VERSION="v1.26.13+k3s2" INSTALL_K3S_EXEC="--node-ip=${IPADDR} --flannel-backend=wireguard-native --node-external-ip=${IPADDR} --flannel-external-ip --disable=traefik --disable=local-storage" K3S_TOKEN=SECRET sh -s - server --server https://${LBIP}:6443 --disable servicelb
+curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="777" INSTALL_K3S_VERSION="v1.26.13+k3s2" INSTALL_K3S_EXEC="--node-ip=${IPADDR} --flannel-backend=wireguard-native --node-external-ip=${IPADDR} --flannel-external-ip --flannel-iface=eth1 --disable=traefik --disable=local-storage" K3S_TOKEN=SECRET sh -s - server --server https://${LBIP}:6443 --disable servicelb
 
 #curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="777" INSTALL_K3S_VERSION="v1.26.13+k3s2" INSTALL_K3S_EXEC="--node-ip=${IPADDR} --disable=traefik" K3S_TOKEN=SECRET sh -s - server --server https://${LBIP}:6443 --disable servicelb
 
