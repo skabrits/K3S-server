4eeb9e582ac2047b4749f3c5de198af050031195
skabrits <seva.kabrits@gmail.com>
2024-10-31 16:09:09 +0300

    Update boss.sh

--- vms/scripts/boss.sh
+++ vms/scripts/boss.sh
@@ -17,7 +17,7 @@ sudo chmod u+w /etc/resolv.conf
 
 sudo echo "nameserver $(echo $IPADDR | cut -d "." -f1-3).1" >> /etc/resolv.conf
 
-curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="777" INSTALL_K3S_VERSION="v1.26.13+k3s2" K3S_TOKEN=SECRET INSTALL_K3S_EXEC="--node-ip=${IPADDR} --flannel-backend=wireguard-native --node-external-ip=${IPADDR} --flannel-external-ip --flannel-iface=eth1 --disable=traefik --disable=local-storage" sh -s - server --cluster-init --tls-san "$(cat /vagrant/LB_IP)" --disable servicelb
+curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="777" INSTALL_K3S_VERSION="v1.31.1+k3s1" K3S_TOKEN=SECRET INSTALL_K3S_EXEC="--node-ip=${IPADDR} --flannel-backend=wireguard-native --node-external-ip=${IPADDR} --flannel-external-ip --flannel-iface=eth1 --disable=traefik --disable=local-storage" sh -s - server --cluster-init --tls-san "$(cat /vagrant/LB_IP)" --disable servicelb
 
 # curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="777" INSTALL_K3S_VERSION="v1.26.13+k3s2" K3S_TOKEN=SECRET INSTALL_K3S_EXEC="--node-ip=${IPADDR} --disable=traefik" sh -s - server --cluster-init --tls-san "$(cat /vagrant/LB_IP)" --disable servicelb
 
